2a6257618f0739119363fb62a0c6363ff4c96153
ufgtftcbtcut <103160162+ufgtftcbtcut@users.noreply.github.com>
2022-12-12 12:48:50 +0700

    Create classes.js

---
+++ classes.js
@@ -0,0 +1,9 @@
+class Nation{
+    navName = "Nations";
+    constructor(icon,region,content) {
+        this.icon = icon;
+        this.region = region;
+        this.content = content;
+        this.displayName = `Nation in ${region}`;
+    }
+}
